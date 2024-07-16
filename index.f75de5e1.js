562e281ee22bb82182d54643661b157e6d480c23
Slava Filimonenko <=>
2024-07-16 21:47:37 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const mainUl = document.createElement("ul");
+    for (const [key, value] of Object.entries(data)){
+        const liElement = document.createElement("li");
+        liElement.textContent = key;
+        mainUl.appendChild(liElement);
+        if (typeof value === "object" && Object.keys(value).length > 0) createTree(liElement, value);
+    }
+    element.appendChild(mainUl);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
